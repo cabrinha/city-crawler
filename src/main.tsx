cf7af78265f8453cb3a3288388175ac397a8f221
cabrinha <yuppie@hey.com>
2025-06-03 08:23:50 -0700

    browser-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+
 
 // Function to update meta tags dynamically
 function updateMetaTags(page: 'home' | 'rankings' = 'home') {
@@ -53,6 +55,8 @@ export { updateMetaTags };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
