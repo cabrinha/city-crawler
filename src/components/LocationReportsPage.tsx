21605304f170f8f77e1b35e4350a46fe0d249454
cabrinha <yuppie@hey.com>
2025-08-31 16:04:05 -0700

    timer

--- src/components/LocationReportsPage.tsx
+++ src/components/LocationReportsPage.tsx
@@ -132,8 +132,9 @@ export const LocationReportsPage: React.FC<LocationReportsPageProps> = ({ onBack
       // Next is today at 22:40
       nextShopExpiration.setUTCHours(22, 40, 0, 0);
     } else {
-      // Next is tomorrow at 10:40
-      nextShopExpiration.setUTCDate(nextShopExpiration.getUTCDate() + 1);
+      // Next is tomorrow at 10:40 - use more robust date calculation
+      const tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+      nextShopExpiration.setUTCFullYear(tomorrow.getUTCFullYear(), tomorrow.getUTCMonth(), tomorrow.getUTCDate());
       nextShopExpiration.setUTCHours(10, 40, 0, 0);
     }
 
